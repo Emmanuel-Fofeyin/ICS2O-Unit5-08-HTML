4463dd73e7db5230241b443845aad500442f4b50
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-01 05:30:36 +0000

    yes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-// Copyright (c) 2022 Marshall All rights reserved
+// Copyright (c) 2022 Emmanuel-Fofeyin All rights reserved
 //
-// Created by: Marshall
+// Created by: Emmanuel-Fofeyin
 // Created on: May 2022
 // This file contains the JS functions for index.html
 
